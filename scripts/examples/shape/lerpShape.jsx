f75d0c53f948de6957b9193e59481799766eaaa2
trych <timorychert@23x5.net>
2016-07-19 14:43:27 +0200

    added lerpShape.jsx script

---
+++ scripts/examples/shape/lerpShape.jsx
@@ -0,0 +1,12 @@
+// for testing the new lerpShape function
+// should later become an example script
+
+#includepath "~/Documents/;%USERPROFILE%Documents";
+#include "basiljs/bundle/basil.js";
+
+function draw() {
+
+
+}
+
+b.go();
